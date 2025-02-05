8bfa06c6153932322a8e9f18b72a49fd9abad790
= <=>
2025-02-05 11:00:29 +0500

    Day 8.4: useRef intro and basics done

--- react-demos/src/App.tsx
+++ react-demos/src/App.tsx
@@ -1,8 +1,18 @@
-import Counter from "./components/Counter"
+import { useRef } from "react"
 
 const App = () => {
+  const inputRef = useRef(null)
+  
+  const focusInput = () => {
+    inputRef.current.focus()
+    inputRef.current.value = 'Hamza Ashraf'
+  }
+
   return (
-    <Counter/>
+  <div>
+     <input type="text" ref={inputRef}/>
+     <button onClick={focusInput}>Focus & Write Hamza</button>
+  </div>
   )
 }
 
