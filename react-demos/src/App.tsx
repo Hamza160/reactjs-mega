a7249bb3a68d3a63e18d4b42df75663f3f54deba
= <=>
2025-02-05 10:40:31 +0500

    Day 8.2: Reducer Function Basics Done

--- react-demos/src/App.tsx
+++ react-demos/src/App.tsx
@@ -1,13 +1,33 @@
-import UpdateUser from "./components/UpdateUser"
-import UserProfile from "./components/UserProfile"
-import { UserProvider } from "./UserContext"
+import { useReducer } from "react"
+
+interface InitialStateInterface {
+  count: number;
+}
+
+const initialState:InitialStateInterface = { count:0 }
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'increment':
+      return {...state, count:state.count + 1}
+    case 'decrement':
+      return {...state, count: state.count - 1}
+    case 'reset':
+      return {...state, count: 0}
+    default:
+      return state
+  }
+}
 
 const App = () => {
+  const [state, disptach] = useReducer(reducer, initialState)
   return (
-    <UserProvider>
-      <UserProfile/>
-      <UpdateUser/>
-    </UserProvider>
+    <div>
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => disptach({type:'increment'})}>Increment</button>
+      <button onClick={() => disptach({type:'decrement'})}>Decrement</button>
+      <button onClick={() => disptach({type:'reset'})}>Reset</button>
+    </div>
   )
 }
 
