b118def1f1f5b739a1abe663428daa6662e89129
= <=>
2025-02-11 13:47:31 +0500

    Day 12: Project Completed

--- react-demos/src/App.tsx
+++ react-demos/src/App.tsx
@@ -5,17 +5,54 @@ import Recommended from "./Recommended/Recommended"
 import Sidebar from "./Sidebar/Sidebar"
 import "./index.css"
 import { products } from "./db/data"
+import Card from "./components/Card"
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null)
   const [query, setQuery] = useState("")
 
   const handleInputChange = e => setQuery(e.target.value)
+  
   const filteredItems = products.filter(product => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1)
 
   // Radio Filters
   const handleChange = e => {
     setSelectedCategory(e.target.value)
+  } 
+
+  // Button Filters
+  const handleClick = e => {
+    setSelectedCategory(e.target.value)
+  }
+
+  function filteredData(products, selected, query){
+    let filteredProducts = products
+
+    // Filtering Input Items
+    if(query){
+      filteredProducts = filteredItems
+    }
+
+    // Selected Filter
+    if(selected){
+      filteredProducts = filteredProducts.filter(({category, color, company, newPrice, title}) => category === selected || 
+      color === selected || 
+      company === selected || 
+      newPrice === selected || 
+      title === selected)
+    }
+
+    return filteredData.map(({img, title, star, reviews, newPrice}) => (
+      <Card 
+        key={Math.random()}
+        img={img}
+        title={title}
+        start={star}
+        reviews={reviews}
+        newPrice={newPrice}
+      />
+    ))
+
   }
 
   return (
