56b4cec6a631d3531b138e01f45a38eada1c973d
= <=>
2025-02-18 11:01:14 +0500

    Day 14.3: Typescript:  Functions Annotations Done

--- ts-turorial/index.ts
+++ ts-turorial/index.ts
@@ -1,5 +1,30 @@
-// Type Inference
-let tech = 'TypeScript'
-let favNumber = 8
-let tsHard = false
-console.log(typeof tech)
+// Function parameters Annotations
+
+// Regular Func
+function addOne(num:number){
+    return num + 1
+}
+
+const res = addOne(3)
+console.log(res)
+
+// Arrow Funct
+const double = (x:number, y:number) => x * y
+const res1 = double(10, 3)
+
+// Default Params Value
+function greet(person: string = 'Annonymous') {
+    return `Hello ${person}`
+}
+
+const res2 = greet()
+console.log(res2)
+
+// Function Return Annotations
+// Regular Func 
+function double1(x:number):number{
+    return x * x
+}
+
+// Arrow Func
+const double2 =(x:number) :number => x * x
