0dc2ab3e0ba119f4858c8768da45a7c956aeaab0
= <=>
2025-02-18 10:31:10 +0500

    Day 14: Typescript:  Type Annotations Done

--- ts-turorial/index.ts
+++ ts-turorial/index.ts
@@ -4,4 +4,13 @@
 let myName: string = 'Hamza Ashraf';
 console.log(myName)
 
-export {}
+// Numbers
+let favNumber: number = 8
+console.log(favNumber)
+
+//  Boolean
+let isHard: boolean = false 
+isHard = true
+
+console.log(isHard)
+
